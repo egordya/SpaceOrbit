cc60b51afac0696e2f05631fed1386c4d4fa026f
Weppan <93657080+Weppan@users.noreply.github.com>
2022-09-06 11:56:46 +0200

    Jag lekte med swing

--- src/main/java/com/grupp7/spaceorbit/SOMain.java
+++ src/main/java/com/grupp7/spaceorbit/SOMain.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class SOMain extends Application {
@@ -15,7 +18,9 @@ public class SOMain extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        final int delay = 50;
     }
+
 
     public static void main(String[] args) {
         launch();
