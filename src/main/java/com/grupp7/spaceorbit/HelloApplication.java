fd91965d763af4d016e1f69d158d96c97b933e1e
oscarwir <skofta@live.se>
2022-09-08 17:53:09 +0200

    k

--- src/main/java/com/grupp7/spaceorbit/HelloApplication.java
+++ src/main/java/com/grupp7/spaceorbit/HelloApplication.java
@@ -27,9 +27,9 @@ public class HelloApplication extends Application {
 
         NormalCelestialObject[] planteter = new NormalCelestialObject[3];
 
-        planteter[0] = new NormalCelestialObject(new Vector2D(0, 0.8), new Vector2D(0.4,0), 1000000);
-        planteter[1] = new NormalCelestialObject(new Vector2D(0, -1.5), new Vector2D(-0.3, 0), 1000000);
-        planteter[2] = new NormalCelestialObject(new Vector2D(0, 0.001), new Vector2D(0, 0), 2100000000);
+        planteter[0] = new NormalCelestialObject(new Vector2D(0, 0.8), new Vector2D(0.2,0), 10500);
+        planteter[1] = new NormalCelestialObject(new Vector2D(0, -1.5), new Vector2D(-0.3, 0), 10000);
+        planteter[2] = new NormalCelestialObject(new Vector2D(0, 0.001), new Vector2D(0, 0), 2200000000.0);
 
         GravitationModel test = new GravitationModel(planteter);
 
@@ -38,8 +38,8 @@ public class HelloApplication extends Application {
         ArrayList<double[]> obj2 = new ArrayList<>();
         ArrayList<double[]> obj3 = new ArrayList<>();
 
-        for (int i = 0; i < 300000; i++) {
-            ArrayList<ObjectForGravitationModel> objekts = test.doSimulationStep(0.005);
+        for (int i = 0; i < 300000 * 5; i++) {
+            ArrayList<ObjectForGravitationModel> objekts = test.doSimulationStep(0.001);
 
             double[] obj1pos = {objekts.get(0).getPos().getX(), objekts.get(0).getPos().getY()};
             double[] obj2pos = {objekts.get(1).getPos().getX(), objekts.get(1).getPos().getY()};
