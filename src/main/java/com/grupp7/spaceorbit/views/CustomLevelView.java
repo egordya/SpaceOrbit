467ccbeafcff09f34b12dd558afc9a2732fdeb39
Maximilian <maxygdell@protonmail.com>
2022-10-17 18:53:01 +0200

    Custom Levels now works. Also added graphics created for Player Objects.

--- src/main/java/com/grupp7/spaceorbit/views/CustomLevelView.java
+++ src/main/java/com/grupp7/spaceorbit/views/CustomLevelView.java
@@ -1,24 +1,26 @@
 package com.grupp7.spaceorbit.views;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import model.customLevelModels.CustomLevelModel;
 
-import javax.json.*;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.Writer;
+import java.util.Objects;
 
 
 public class CustomLevelView extends AnchorPane {
     CustomLevelModel cml = new CustomLevelModel();
+
+    @FXML
+    ListView<String> objectsListView = new ListView<String>();
+
     @FXML
     private AnchorPane addObjectCustomLevelAnchorPane;
 
@@ -35,6 +37,12 @@ public class CustomLevelView extends AnchorPane {
     private TextField nameInput;
 
     @FXML
+    private TextField velYInput;
+
+    @FXML
+    private TextField velXInput;
+
+    @FXML
     private ImageView planetPreview;
 
     @FXML
@@ -47,7 +55,7 @@ public class CustomLevelView extends AnchorPane {
     private TextField startYInput;
 
     @FXML
-    private ComboBox<Boolean> staticComboBoxInput;
+    private ComboBox<String> staticComboBoxInput;
 
     @FXML
     private ChoiceBox<String> typeChoiceBox;
@@ -60,11 +68,21 @@ public class CustomLevelView extends AnchorPane {
 
     @FXML
     private Label levelNameLabel;
+
+    @FXML
+    private Label objectAdded;
+
+    @FXML
+    private Label levelNameMissing;
+
+    @FXML
+    private Label levelAddedLabel;
+
     String levelName;
     Mediator mediator;
+    ObservableList<String> objectList = FXCollections.observableArrayList();
 
     public CustomLevelView(Mediator mediator) {
-
         this.mediator = mediator;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/customlevel.fxml"));
         fxmlLoader.setRoot(this);
@@ -77,9 +95,13 @@ public class CustomLevelView extends AnchorPane {
         }
     }
 
+    /**
+     * @throws IOException
+     * Gathers user input data from GUI, checks if all mandatory parameters are set.
+     * Adds type and starting positions to list of objects in objectsListView.
+     */
     @FXML
     public void submitButton() throws IOException {
-
         String name = nameInput.getText();
         int mass = Integer.parseInt(massInput.getText());
         int radius = Integer.parseInt(radiusInput.getText());
@@ -87,27 +109,70 @@ public class CustomLevelView extends AnchorPane {
         double posY = Double.parseDouble(startYInput.getText());
         String imagePath = imageComboBoxInput.getValue();
         String type = typeChoiceBox.getValue();
-        String isFixed = staticComboBoxInput.getValue().toString();
-        cml.createJsonObject(name, mass, radius, posX, posY, imagePath, type, isFixed);
+        Boolean isFixed = Boolean.valueOf(staticComboBoxInput.getValue());
+        double velX = Double.parseDouble(velXInput.getText());
+        double velY = Double.parseDouble(velYInput.getText());
+
+        if(!Objects.equals(name, "") && type != null && isFixed && imagePath != null) {
+            cml.createJsonObject(name, mass, radius, posX, posY, velX, velY, imagePath, type, isFixed);
+            objectAdded.setText(name + " added!");
+            objectList.add(name + "  Type:   " + type + "    Start position(x,y):   (" + posX + " , " + posY + ")");
+        }else{
+            objectAdded.setText("Missing parameters!");
+        }
     }
 
+    /**
+     * @throws IOException
+     * Sends gathered data to model.
+     * If cml.levelBuilt returns true, confirms that level json is added.
+     */
     @FXML
-    public void submitButtonTwo() throws IOException {
-        cml.finishAll(levelName);
+    public void createLevel() throws IOException {
+        cml.createJson(levelName);
+        if(cml.levelBuilt){
+            levelAddedLabel.setText("Your level was added to the Game. Enjoy!");
+        }else{
+            levelAddedLabel.setText("You are missing required objects (Planet, Target, Player)");
+        }
     }
 
+
+    /**
+     * @throws IOException
+     * Sets name of level.
+     * Used for naming json file.
+     */
     @FXML
     public void nameNewLevel() throws IOException {
         levelName = levelNameTextField.getText();
-        levelNameLabel.setText(levelName);
-        nameCustomLevelAnchorPane.toBack();
+        levelName = levelName.replace(" ", "_");
+        if(!levelName.equals("")) {
+            levelNameLabel.setText(levelName);
+            nameCustomLevelAnchorPane.toBack();
+            levelNameMissing.setText("");
+        }else{
+            levelNameMissing.setText("Please write a name for your level! ");
+        }
     }
 
     @FXML
-    public void addObject() throws IOException{
-        levelOverviewAnchorPane.toBack();
+    public void showAddObjectListPane() throws IOException{
+        addObjectCustomLevelAnchorPane.toFront();
     }
 
+    @FXML
+    public void backToList() throws IOException{
+        objectsListView.setItems(objectList);
+        levelOverviewAnchorPane.toFront();
+    }
+
+    @FXML
+    public void backToMainMenu() throws FileNotFoundException {
+        objectsListView.toFront();
+        mediator.notify(this, MediatorCommand.STANDARD);
+
+    }
     @Override
     public Node getStyleableNode() {
         return super.getStyleableNode();
--- src/main/java/model/gameModel/GameModel.java
+++ src/main/java/model/gameModel/GameModel.java
@@ -67,7 +67,6 @@ public class GameModel extends AGameModel{
 
     private void handleCollisions() {
 
-
         collisionModel.checkCollisions(getAllCelestialObjects());
         Collisionable[][] pairs = collisionModel.getCollidedPairs();
 
